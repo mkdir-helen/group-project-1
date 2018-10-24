32760c262ff8449a33ab33f547039408a0e373a0
Clare Barton <grimalkinclare@gmail.com>
2018-10-24 13:52:36 -0400

    adds array of images and starts modal functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,20 @@
-// this is where the js goes
+const dataContainer = document.querySelector('[data-container]');
+const dataModal = document.querySelector('[data-modal]');
+const thumbnail = document.querySelectorAll('.thumbnail');
+const images = ["images/aries.jpg",
+    "images/taurus.jpg",
+    "images/gemini.jpg",
+    "images/cancer.jpg",
+    "images/leo.jpg",
+    "images/virgo.jpg",
+    "images/libra.jpg",
+    "images/scorpio.jpg",
+    "images/sagittarius.jpg",
+    "images/capricorn.jpg",
+    "images/aquarius.jpg",
+    "images/pisces.jpg"];
+
+thumbnail.addEventListener('click', function (event) {
+    event.target.
+        console.log("hello");
+});
