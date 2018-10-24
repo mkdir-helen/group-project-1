8c2381e5db0df21d955a10e074143994efff860b
Clare Barton <grimalkinclare@gmail.com>
2018-10-24 15:38:14 -0400

    got the zodiac characteristics right... finally

--- scripts/astro.js
+++ scripts/astro.js
@@ -1,19 +1,29 @@
+{
+    _id: "5973d0fd4d75a100114e1d79",
+        name: "Aries",
+            __v: 0,
+                famous_people: Array(32),
+                    how_to_spot: Array(3),
+    …
+}
+
+
 const astro_url = "http://my-little-cors-proxy.herokuapp.com/https://zodiacal.herokuapp.com/api";
 
 function zodiacData() {
     fetch(astro_url)
-    .then(r => r.json())
-    .then(cacheData)
-    .then(doStuff)
+        .then(r => r.json())
+        .then(cacheData)
+        .then(doStuff)
 }
 
-function cacheData(data){
+function cacheData(data) {
     //store data
     return data;
 }
 
-function doStuff(data){
-    console.log(data[0]);
+function doStuff(data) {
+    console.log(data);
 }
 
 zodiacData();
--- scripts/index.js
+++ scripts/index.js
@@ -14,12 +14,53 @@ const images = ["images/aries.jpg",
     "images/aquarius.jpg",
     "images/pisces.jpg"];
 
+const astro_url = "http://my-little-cors-proxy.herokuapp.com/https://zodiacal.herokuapp.com/api";
 
-// console.log(thumbnail.length);
-for (let i = 0; i < thumbnail.length; i++) {
-    // console.log('hello');
-    thumbnail[i].addEventListener('click', function () {
-        // to do shit;
-        // dataModal.remove(modal - hidden);
-    });
-};
+function zodiacData() {
+    fetch(astro_url)
+        .then(r => r.json())
+        .then(cacheData)
+        .then(doStuff)
+        .then(retrieve)
+}
+
+function cacheData(data) {
+    //store data
+    return data;
+}
+
+function doStuff(data) {
+    console.log(data);
+    return data;
+}
+
+zodiacData();
+
+
+function retrieve(data) {
+    for (let i = 0; i < thumbnail.length; i++) {
+        thumbnail[i].addEventListener('click', function () {
+            if (data[i].name) {
+                dataModal.innerHTML = '';
+                let elements = [data[i].mental_traits[0], data[i].secret_wish, data[i].vibe,
+                data[i].physical_traits[0], data[i].element, data[i].famous_people[0], data[i].hates[0],
+                data[i]['compatibility']];
+
+                elements.forEach(function (stuff) {
+                    let newP = document.createElement('p');
+                    newP.textContent = stuff;
+                    dataModal.appendChild(newP);
+
+                })
+                // console.log(data[i].mental_traits[0]);
+                // let newP = document.createElement('p');
+                // let newQ = document.createElement('p');
+                // newP.textContent = (`Your mental traits are ${data[i].mental_traits[0]}.`);
+                // newQ.textContent = (`Your secret wish is ${data[i].secret_wish[0]}.`);
+                // // console.log(newP);
+                // dataModal.appendChild(newQ);
+                // dataModal.remove(modal - hidden);
+            }
+        })
+    }
+}
