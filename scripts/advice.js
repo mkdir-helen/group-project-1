f8cfd8742235d237069e5b40b61875baf0b2b510
Clare Barton <grimalkinclare@gmail.com>
2018-10-25 12:36:53 -0400

    adds ajax for advice slip

---
+++ scripts/advice.js
@@ -0,0 +1,15 @@
+// {
+//     "slip":
+//     {
+//         "advice": "True happiness always resides in the quest.",
+//             "slip_id": "9"
+//     }
+// }
+
+
+function adviceSlip() {
+    fetch('http://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(data => console.log(data.slip.advice))
+}
+adviceSlip();
